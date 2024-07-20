64e4361e7ac4207cb7f0687a0b2202b75e674b67
armstrongcorner <39514373+armstrongcorner@users.noreply.github.com>
2024-07-20 22:22:45 +1200

    init commit

---
+++ sidureader/sidureaderApp.swift
@@ -0,0 +1,17 @@
+//
+//  sidureaderApp.swift
+//  sidureader
+//
+//  Created by Armstrong Liu on 20/07/2024.
+//
+
+import SwiftUI
+
+@main
+struct sidureaderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
